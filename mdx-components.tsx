0dc1b067831ed39265add5af9bcfbbbf37c17db5
Xiangzhuang Shen <datahonor@gmail.com>
2024-05-04 23:12:09 +0800

    add: mdx init

---
+++ mdx-components.tsx
@@ -0,0 +1,14 @@
+import type { MDXComponents } from 'mdx/types'
+
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    // Allows customizing built-in components, e.g. to add styling.
+    h1: ({ children }) => (
+      <h1 style={{ color: 'black', fontSize: '30px', textAlign: 'center'}}>{children}</h1>
+    ),
+    h2: ({ children }) => <h2 className="text-3xl font-bold text-center">{children}</h2>,
+
+    ...components,
+  }
+}
